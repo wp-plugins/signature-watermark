a6cf10917a19f8ce438b536b2bcd7ca4f7454f37
ChrisHurst <ChrisHurst@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-15 14:39:02 +0000

    tagging v1.5
    git-svn-id: https://plugins.svn.wordpress.org/signature-watermark/tags/1.5@625707 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-loader.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Signature Watermark
+Plugin URI: http://MyWebsiteAdvisor.com/tools/wordpress-plugins/signature-watermark/
+Description: Add transparent PNG image and text signature watermark to your uploaded images.
+Version: 1.5
+Author: MyWebsiteAdvisor
+Author URI: http://MyWebsiteAdvisor.com
+*/
+
+register_activation_hook(__FILE__, 'signature_watermark_activate');
+
+// display error message to users
+if ($_GET['action'] == 'error_scrape') {                                                                                                   
+    die("Sorry, Signature Watermark Plugin requires PHP 5.0 or higher. Please deactivate Signature Watermark Plugin.");                                 
+}
+
+function signature_watermark_activate() {
+	if ( version_compare( phpversion(), '5.0', '<' ) ) {
+		trigger_error('', E_USER_ERROR);
+	}
+}
+
+// require Signature Watermark Plugin if PHP 5 installed
+if ( version_compare( phpversion(), '5.0', '>=') ) {
+	define('SW_LOADER', __FILE__);
+
+	require_once(dirname(__FILE__) . '/signature-watermark.php');
+	require_once(dirname(__FILE__) . '/plugin-admin.php');
+	
+	$watermark = new Signature_Watermark_Admin();
+
+}
+?>
